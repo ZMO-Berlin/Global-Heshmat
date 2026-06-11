fc0904ad9a769cc0df16ec0cd73181ef49ef6029
janpu <jan.purtzel@zmo.de>
2026-06-11 12:24:51 +0200

    changes 1106

--- svelte-app/src/lib/data/artworks/024-Fachschule für Porzellan.ts
+++ svelte-app/src/lib/data/artworks/024-Fachschule für Porzellan.ts
@@ -13,20 +13,16 @@ const artwork: Artwork = {
 	status: 'located',
 	address: 'Weißenbacher Str. 60, 95100 Selb',
 	desc: 'In 1957–58, Hassan Heshmat studied at the "Staatliche Höhere Fachschule für Porzellan" in Selb on a DAAD scholarship.',
-	images: [{src: "Selb_Haus_I.jpeg",
-	         caption: "Hassan Heshmats Accodmodation in Selb, Bavaria (taken 2026)"},
-		{src: "Selb_Haus_II.jpeg",
-	         caption: "Hassan Heshmats Accodmodation in Selb, Bavaria (taken 2026)"},
-			 {src: "OFRA_Okt_74.tif",
+	images: [{src: "Schule_2026.jpg",
+	         caption: "A recent photo of the school from 2026"},
+		{src: "OFRA_Okt_74.tif",
 	         caption: "October 1974"},
 		{src: "Schulgebäude_bis_1971.jeg",
 	         caption: "School Building until 1971"},
 			 {src: "Wärmetechnisches_Labor.jpg",
 	         caption: "Thermal Engineering Laboratory"},
 			 {src: "Technikerbau_Plan.tif",
-	         caption: "Engineering Construction Plan"},
-			 {src: "Schule_2026.jpg",
-	         caption: "A recent photo of the school from 2026"}	]
+	         caption: "Engineering Construction Plan"}]
 	
 	
 };
--- svelte-app/src/lib/data/artworks/039- Hotel Schmidt Selb.ts
+++ svelte-app/src/lib/data/artworks/039- Hotel Schmidt Selb.ts
@@ -12,7 +12,7 @@ const artwork: Artwork = {
 	city: 'Selb',
 	status: 'located',
 	address: 'Bahnhofstraße 19, 95100 Selb',
-	desc: 'According to an entry in Ahmed Hegazy’s address book, Hassan Heshmat also stayed at the Hotel Schmidt in Selb. The hotel opened in 19xy. It is situated on the route from the railway station to the town centre, about 400 metres from the centre. The Porzellanikon Museum is about 1 km away, and the school is around 750 metres away.',
+	desc: 'According to an entry in Ahmed Hegazy’s address book, Hassan Heshmat also stayed at the Hotel Schmidt in Selb. The hotel opened in 19xy. It is situated on the route from the railway station to the town centre, about 400 metres from the centre. The Porzellanikon Museum is about 3 km away, and the school is around 750 metres away.',
 	images: [{src: "Schmidt_Selb_I.jpeg",
 	         caption: "Entry in the address book of Ahmed Hegazy"},
 		{ 		src: "Schmidt_Selb_II.jpeg",
