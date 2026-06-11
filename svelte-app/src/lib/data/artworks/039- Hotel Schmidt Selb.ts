5c622a65501e586dddfbcef4d5c1ba5cb08733bb
janpu <jan.purtzel@zmo.de>
2026-06-11 12:09:26 +0200

    art work 39

---
+++ svelte-app/src/lib/data/artworks/039- Hotel Schmidt Selb.ts
@@ -0,0 +1,25 @@
+
+
+import type { Artwork } from '../types';
+
+const artwork: Artwork = {
+	
+	id: 39,
+	name: 'Hotel Schmidt',
+	lat: 50.17065270578028,
+	lng: 12.127331675399757,
+	country: 'Germany',
+	city: 'Selb',
+	status: 'located',
+	address: 'Bahnhofstraße 19, 95100 Selb',
+	desc: 'According to an entry in Ahmed Hegazy’s address book, Hassan Heshmat also stayed at the Hotel Schmidt in Selb. The hotel opened in 19xy. It is situated on the route from the railway station to the town centre, about 400 metres from the centre. The Porzellanikon Museum is about 1 km away, and the school is around 750 metres away.',
+	images: [{src: "Schmidt_Selb_I.jpeg",
+	         caption: "Entry in the address book of Ahmed Hegazy"},
+		{ 		src: "Schmidt_Selb_II.jpeg",
+         		caption: "Entry in the address book of Ahmed Hegazy"}]
+
+	
+	
+};
+
+export default artwork;
