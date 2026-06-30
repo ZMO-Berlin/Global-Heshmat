aca4f63eeb317852fc634f1756e031496cf1d017
janpu <jan.purtzel@zmo.de>
2026-06-30 14:15:41 +0200

    changes 3006 II

--- svelte-app/src/lib/data/artworks/011-Sheraton Heliopolis.ts
+++ svelte-app/src/lib/data/artworks/011-Sheraton Heliopolis.ts
@@ -10,6 +10,8 @@ const artwork: Artwork = {
 	status: 'located',
 	address: 'El-Orouba, Sheraton Al Matar, El Nozha, Cairo Governorate 11361',
 	desc: 'In 1980, the three-metre high statue ‘Welcome’ made from granite stone was placed in the lobby of the Heliopolis Sheraton. The Sheraton chain in Egypt had close relations with the artist, as Eugen Auer, General Manager of the Heliopolis Sheraton from 1985-87 relays. In 1988 a fire spread in the hotel, killing 16 people and leaving the 630-bed hotel in ruins. The hotel was closed and only reopened seven years later. Though the statue is shown in the artist’s autobiography after it had been cleaned of traces of the fire, it is not known, where the sculpture stands today.',
+	image: 'sheraton_heliopolis.jpg',
+	imageCaption: 'From his Autobiography: ‘Welcome’ figurine',
 	video: "https://www.youtube.com/watch?v=ZytKel21hh8"
 
 	
