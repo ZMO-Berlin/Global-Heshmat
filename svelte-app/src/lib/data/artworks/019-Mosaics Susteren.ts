7fe0a8a5a4a33a5991732f5ab68af798cce62909
riekemuel <rieke.muelstegen@mv31.de>
2026-07-07 11:57:19 +0200

    Add images and update Susteren artwork description

--- svelte-app/src/lib/data/artworks/019-Mosaics Susteren.ts
+++ svelte-app/src/lib/data/artworks/019-Mosaics Susteren.ts
@@ -11,7 +11,7 @@ const artwork: Artwork = {
 	city: 'Susteren',
 	status: 'located',
 	address: 'Marktstraat 45, 6114 HR Susteren',
-	desc: 'Till 2025 it was unclear were these, very untypical reliefs decorating a private swimming pool were located. After the exhibition ‘Hassan Heshmat: Artist of the people between cultural heritage and renewal’ in 2025 by Divan – Das arabische Kulturhaus in Berlin and ZMO, the granddaughter of the owner got in touch with Sonja Hegasy. Her grandfather, the medical doctor Abe Koldijk (1921-2015) and his wife Theresa Meigen had lived in Cairo in the late 1950s/early 1960s and became very close friends with Hassan Heshmat. Back home, Koldijk organized an exhibition in Susteren in 1968. It was Dr Koldijk who commissioned the mosaics around his private swimming pool. <br>His family sold the house around 2015, and we are currently in touch with the new owners. The mosaic is still in place.',
+	desc: 'Till 2025 it was unclear were these, very untypical reliefs decorating a private swimming pool were located. After the exhibition ‘Hassan Heshmat: Artist of the people between cultural heritage and renewal’ in 2025 by Divan – Das arabische Kulturhaus in Berlin and ZMO, the granddaughter of the former owner got in touch with Sonja Hegasy. Her grandfather, the medical doctor Abe Koldijk (1921-2015) and his wife Theresa Meigen had lived in Cairo in the late 1950s/early 1960s and became very close friends with Hassan Heshmat. Back home, Koldijk organized an exhibition in Susteren in 1968. It was Dr Koldijk who commissioned the mosaics around his private swimming pool. <br>His family sold the house around 2015, and we are currently in touch with the new owners. The mosaic is still in place.',
 	images: [{src: "Küssende_Schwimmbad.jpg"},
 		{ 		src: "Küssende_Schwimmbad_II.jpg"},
 	   {	src: "Schwimmbad_3.jpg"},
@@ -20,8 +20,15 @@ const artwork: Artwork = {
 		   { 		src: "Plakat_Ausstellung_Susteren_1968_Niederlande.jpeg",
          		caption: "Poster for the Exhibition in Susteren 1968"},
 	   {	src: "Rückseite_Schwimmbad.jpg",
-     	   caption: "Backside of a Photo: Part of a 30-square-metre ceramic panel surrounding a swimming pool in the Netherlands, 1970. By Hassan Heshmat"}
-		   ]
+     	   caption: "Backside of a Photo: Part of a 30-square-metre ceramic panel surrounding a swimming pool in the Netherlands, 1970. By Hassan Heshmat"},
+		   {src: "Gänse_Schwimmbad.jpeg"},
+		   {src: "KleineFigur_Schwimmbad.jpeg"},
+		   {src: "Paar_Schwimmbad.jpeg"},
+		   {src: "Paar_Schwimmbad_2.jpeg"},
+		   {src: "Paar_Schwimmbad_3.jpeg"},
+		   {src: "Tanzende_Schwimmbad_2.jpeg"}
+		   
+		]
 
 	};
 
